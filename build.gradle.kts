6018e3df50f9525bbb962d48c4fec67a3ea6b693
DomamaN202 <vip.domaman@mail.ru>
2023-12-12 10:06:34 +0700

    =)

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+    `maven-publish`
+}
+
+group = "ru.DmN.phtx"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    implementation("ru.DmN.siberia:Project-Siberia:1.5.5")
+    implementation("ru.DmN.pht:Project-Pihta:1.3.1")
+    implementation(files("Lazurite-2.7.0.jar"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    build {
+        dependsOn(register<Jar>("fatJar") {
+            dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+            archiveClassifier.set("standalone")
+            duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+            manifest { attributes(mapOf("Main-Class" to "ru.DmN.pht.base.Console")) }
+            val sourcesMain = sourceSets.main.get()
+            val contents = configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) } + sourcesMain.output
+            from(contents)
+        })
+    }
+
+    java {
+        withSourcesJar()
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group as String
+            artifactId = "Project-PLS"
+            version = project.version as String
+            from(components["java"])
+        }
+    }
+}
